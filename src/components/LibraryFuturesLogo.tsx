e19c9113755be57fa5a573e4cf3291b2af3d67e9
Jennie Rose Halperin <little-wow@users.noreply.github.com>
2026-05-26 16:53:29 -0400

    Update LibraryFuturesLogo.tsx

--- src/components/LibraryFuturesLogo.tsx
+++ src/components/LibraryFuturesLogo.tsx
@@ -1,13 +1,13 @@
 import React from "react";
+import logo from "../LIbraryFutures_CMYK.svg";
 
 export function LibraryFuturesLogo() {
   return (
     <div className="flex items-center flex-shrink-0 select-none">
       <img
-        src="https://github.com/little-wow/copyright-tools/blob/main/src/LIbraryFutures_CMYK.svg"
+        src={logo}
         alt="Library Futures Logo"
         className="h-10 sm:h-12 w-auto object-contain transition-transform duration-300 hover:scale-[1.02]"
-        referrerPolicy="no-referrer"
       />
     </div>
   );
